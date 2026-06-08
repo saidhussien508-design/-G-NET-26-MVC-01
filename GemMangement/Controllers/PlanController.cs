ad2e453d741b8e7013044eea5793fbb40d6f6979
3D <3D@DESKTOP-HE1GBV4>
2026-06-08 06:46:42 +0300

    Add project files.

---
+++ GemMangement/Controllers/PlanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GemMangement.Controllers
+{
+    public class PlansController:Controller
+    {
+        private readonly GemAppDpContext _Context=new GemAppDpContext();    
+        public async Task<IActionResult> index()
+        {
+         var plan =await _Context.planes.ToListAsync();
+
+            return View(plan);
+        }
+        public IActionResult Details(int id)
+        {
+           var plan= _Context.planes.FirstOrDefault(p=>p.Id == id);
+            if (plan is null)return RedirectToAction(nameof(Index));
+             return View(plan);
+        }
+    }
+}
---
+++ GemMangement/GemAppDpContext.cs
@@ -0,0 +1,22 @@
+﻿using GemMangement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GemMangement
+{
+    public class GemAppDpContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =.; Database = Gem; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        public DbSet<plane> planes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
